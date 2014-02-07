d407ed85e720c4de7091537e7022d292a3360452
Stas Zhukovskiy <stzhuk@gmail.com>
2014-02-07 14:13:14 +0400

    added squre cropping to UIImageView

--- ZSAdditions/UIImage+ZHAdditions.h
+++ ZSAdditions/UIImage+ZHAdditions.h
@@ -10,9 +10,10 @@
 
 @interface UIImage (ZHAdditions)
 
-+ (UIImage*)imageFromView:(UIView*)view;
-+ (UIImage*)imageFromView:(UIView*)view scaledToSize:(CGSize)newSize;
-+ (UIImage*)imageWithImage:(UIImage*)image scaledToSize:(CGSize)newSize;
++ (UIImage *)imageFromView:(UIView *)view;
++ (UIImage *)imageFromView:(UIView *)view scaledToSize:(CGSize)newSize;
++ (UIImage *)imageWithImage:(UIImage *)image scaledToSize:(CGSize)newSize;
++ (UIImage *)cropBiggestCenteredSquareImageFromImage:(UIImage *)image withSide:(CGFloat)side;
 + (void)beginImageContextWithSize:(CGSize)size;
 
 @end
--- ZSAdditions/UIImage+ZHAdditions.m
+++ ZSAdditions/UIImage+ZHAdditions.m
@@ -50,4 +50,48 @@
     return newImage;
 }
 
++ (UIImage *)cropBiggestCenteredSquareImageFromImage:(UIImage *)image withSide:(CGFloat)side {
+    // Get size of current image
+    CGSize size = image.size;
+    if(size.width == size.height && size.width == side)
+        return image;
+    CGSize newSize = CGSizeMake(side, side);
+    double ratio;
+    double delta;
+    CGPoint offset;
+    
+    //make a new square size, that is the resized imaged width
+    CGSize sz = CGSizeMake(newSize.width, newSize.width);
+    
+    //figure out if the picture is landscape or portrait, then
+    //calculate scale factor and offset
+    if (image.size.width > image.size.height) {
+        ratio = newSize.height / image.size.height;
+        delta = ratio*(image.size.width - image.size.height);
+        offset = CGPointMake(delta/2, 0);
+    } else {
+        ratio = newSize.width / image.size.width;
+        delta = ratio*(image.size.height - image.size.width);
+        offset = CGPointMake(0, delta/2);
+    }
+    
+    //make the final clipping rect based on the calculated values
+    CGRect clipRect = CGRectMake(-offset.x, -offset.y,
+                                 (ratio * image.size.width),
+                                 (ratio * image.size.height));
+    
+    //start a new context, with scale factor 0.0 so retina displays get
+    //high quality image
+    if ([[UIScreen mainScreen] respondsToSelector:@selector(scale)])
+        UIGraphicsBeginImageContextWithOptions(sz, YES, 0.0);
+    else
+        UIGraphicsBeginImageContext(sz);
+    UIRectClip(clipRect);
+    [image drawInRect:clipRect];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
 @end
