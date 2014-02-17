26079805bb2f565387c9b6dfc3b4ac745a2992b5
Stanislav Zhukovskiy <stzhuk@gmail.com>
2014-02-17 12:02:29 +0400

    added general header file

---
+++ ZSAdditions/ZSAdditions.h
@@ -0,0 +1,13 @@
+//
+//  ZSAdditions.h
+//  
+//
+//  Created by Stanislav Zhukovskiy on 17.02.14.
+//  Copyright (c) 2014 Stas Zhukovskiy. All rights reserved.
+//
+
+#import "ZSAdditions/UIImage+ZSAdditions.h"
+
+#import "ZSAdditions/NSArray+ZSAdditions.h"
+#import "ZSAdditions/NSMutableArray+ZSAdditions.h"
+#import "ZSAdditions/NSString+ZSAdditions.h"
