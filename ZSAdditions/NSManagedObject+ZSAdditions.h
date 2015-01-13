7478585c88f9c19385093707b72dce623998167f
Stas Zhukovskiy <stzhuk@gmail.com>
2015-01-13 23:36:28 +0300

    Added an NSManagedObjectSubclass with handy methods to make Core Data life easier

---
+++ ZSAdditions/NSManagedObject+ZSAdditions.h
@@ -0,0 +1,16 @@
+//
+//  NSManagedObject+ZSAdditions.h
+//  
+//
+//  Created by Stas Zhukovskiy on 12.01.15.
+//  Copyright (c) 2015 Stas Zhukovskiy. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+@interface NSManagedObject (EntityName)
+
++ (NSString *)entityName;
++ (instancetype)newInstanceInManagedObjectContext:(NSManagedObjectContext *)context;
+
+@end
---
+++ ZSAdditions/NSManagedObject+ZSAdditions.m
@@ -0,0 +1,23 @@
+//
+//  NSManagedObject+ZSAdditions.m
+//
+//
+//  Created by Stas Zhukovskiy on 12.01.15.
+//  Copyright (c) 2015 Stas Zhukovskiy. All rights reserved.
+//
+
+#import "NSManagedObject+ZSAdditions.h"
+
+@implementation NSManagedObject (EntityName)
+
++ (NSString *)entityName {
+    return NSStringFromClass(self);
+}
+
++ (instancetype)newInstanceInManagedObjectContext:(NSManagedObjectContext *)context {
+    id object = [NSEntityDescription insertNewObjectForEntityForName:[self entityName]
+                                              inManagedObjectContext:context];
+    return object;
+}
+
+@end
