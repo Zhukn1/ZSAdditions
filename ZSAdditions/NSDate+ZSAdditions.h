80f4abc4504cd211e386c04f82e6b357d6c8fd7b
Stas Zhukovskiy <stzhuk@gmail.com>
2015-01-23 14:23:52 +0300

    added an NSDate category

---
+++ ZSAdditions/NSDate+ZSAdditions.h
@@ -0,0 +1,13 @@
+//
+//  NSDate+ZSAdditions.h
+//  
+//
+//  Created by Stanislav Zhukovskiy on 23.01.15.
+//  Copyright (c) 2015 Stas Zhukovskiy. All rights reserved.
+//
+
+@interface NSDate (ZSAdditions)
+
+- (NSDate *)normalizedDate:(NSDate *)date;
+
+@end
---
+++ ZSAdditions/NSDate+ZSAdditions.m
@@ -0,0 +1,30 @@
+//
+//  NSDate+ZSAdditions.m
+//
+//
+//  Created by Stanislav Zhukovskiy on 23.01.15.
+//  Copyright (c) 2015 Stas Zhukovskiy. All rights reserved.
+//
+
+#import "NSDate+ZSAdditions.h"
+
+@implementation NSDate (ZSAdditions)
+
+- (NSDate *)normalizedDate:(NSDate *)date {
+    
+    NSCalendar *gregorian = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    [gregorian setTimeZone:[NSTimeZone localTimeZone]];
+    
+    unsigned unitFlags = NSCalendarUnitDay | NSCalendarUnitMonth | NSCalendarUnitYear | NSCalendarUnitWeekday | kCFCalendarUnitWeekdayOrdinal;
+    NSDateComponents *components = [[NSCalendar currentCalendar] components:unitFlags fromDate:date];
+    
+    NSDateComponents *normalized = [NSDateComponents new];
+    [normalized setWeekday: [components weekday]];
+    [normalized setWeekdayOrdinal:[components weekdayOrdinal]];
+    [normalized setMonth:[components month]];
+    [normalized setYear:[components year]];
+    
+    return [gregorian dateFromComponents:normalized];
+}
+
+@end
